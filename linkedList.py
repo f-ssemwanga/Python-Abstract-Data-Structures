f052a07bc737835bd5b563707e071022ae1e4649
f-ssemwanga <dssales@gmx.com>
2023-03-08 21:38:47 +0000

    added the append function to add a node to the linked list

--- linkedList.py
+++ linkedList.py
@@ -11,5 +11,23 @@ class LinkedList():
     #the head node will contain no data
     def __init__(self):
         self.head = Node() # allows us to point to the first element in the list
+
+    def appendFunc(self, data):
+        #this will add a new data point to the list
+        new_node = Node()
+        curr = self.head #as there are no nodes the current node is the head node
+        #interate over notes in the list until we find the next node of curr which is None
+        #we then insert the new node at this new location
+        while curr.next !=None:
+            #traverse through the list
+            curr = curr.next
+        #once we are at the last node make the new node the next node of the current node
+        curr.next = new_node
+        #be sure to text the code at this stage
+    
+        
+
 myLinkedList = LinkedList()
-print(myLinkedList.head)
+myLinkedList.appendFunc(5)
+myLinkedList.appendFunc(22)
+print(myLinkedList)
