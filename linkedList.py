9cf462233321b107463acad50975cae0613c5938
f-ssemwanga <dssales@gmx.com>
2023-03-08 21:28:56 +0000

    linked list constructor added

--- linkedList.py
+++ linkedList.py
@@ -6,3 +6,10 @@ class Node():
         #data is the data point to be stored in the node
         self.data  = data
         self.next = None # pointer to the next node - last node's next pointer is None by default
+class LinkedList():
+    #we will need a head node
+    #the head node will contain no data
+    def __init__(self):
+        self.head = Node() # allows us to point to the first element in the list
+myLinkedList = LinkedList()
+print(myLinkedList.head)
