a50051cc00901d5dcd9ac729eb2ad30c9712b926
f-ssemwanga <dssales@gmx.com>
2023-03-08 22:04:30 +0000

    display method and testing the linked list so far

--- linkedList.py
+++ linkedList.py
@@ -14,20 +14,32 @@ class LinkedList():
 
     def appendFunc(self, data):
         #this will add a new data point to the list
-        new_node = Node()
-        curr = self.head #as there are no nodes the current node is the head node
+        new_node = Node(data)
+        current = self.head #as there are no nodes the current node is the head node
         #interate over notes in the list until we find the next node of curr which is None
         #we then insert the new node at this new location
-        while curr.next !=None:
+        while current.next != None:
             #traverse through the list
-            curr = curr.next
+            current = current.next
         #once we are at the last node make the new node the next node of the current node
-        curr.next = new_node
-        #be sure to text the code at this stage
+        current.next = new_node
+        
+    def display(self):
+        #helper function to display the contents of the linked list
+        visitedElements = [] # store elements in the list
+        current = self.head # traverse the list starting with the head node
+        while current.next !=None:
+            current = current.next
+            visitedElements.append(current.data)
+        print(visitedElements)
+ 
+        
+        
     
         
 
-myLinkedList = LinkedList()
-myLinkedList.appendFunc(5)
-myLinkedList.appendFunc(22)
-print(myLinkedList)
+myList = LinkedList()
+myList.display()
+myList.appendFunc(5)
+myList.appendFunc(22)
+myList.display()
