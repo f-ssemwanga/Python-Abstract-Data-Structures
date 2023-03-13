e2119134a81ca53df364486fc2a88f4011ed4ed2
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-03-13 06:30:30 +0000

    added peek and dequeue operations

--- stackOopImplementation.py
+++ stackOopImplementation.py
@@ -7,5 +7,19 @@ class Stack:
         self.stack = []
     def push(self,data):
         #adds element on to the stack
-        self.stack.append(data)
-    
+        if data not in self.stack:
+            self.stack.append(data)
+            return True
+        else:
+            return False
+    def peek(self):
+        #returns the element on top of the stack
+        return self.stack[-1]
+    def dequeue(self):
+        #removes peek element
+        #check if the stack is not emipty
+        if len(self.stack) <= 0:
+            return "Stack is empty"
+        else:
+            removedElement = self.stack.pop()
+            return f'removed: {removedElement}'
