af819eb7f905ebb8fc1e14d459a8c6bb1255410c
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-03-13 06:06:12 +0000

    append and size operations

--- stackUsingList.py
+++ stackUsingList.py
@@ -1,2 +1,16 @@
 #stack implementation using a list
 stack = []
+
+#push operation - we will use the append method
+stack.append(5)
+stack.append(6)
+stack.append(9)
+stack.append(6)
+stack.append(3)
+
+print('Current Stack')
+print(stack)
+#get the size of the stack
+print(f'The stack has {len(stack)} elements')
+
+
